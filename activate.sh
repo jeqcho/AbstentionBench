6716195cc64ab0474633d84654d9ba976df1e4c7
Samuel Bell <samueljamesbell@gmail.com>
2025-06-26 17:33:34 +0100

    Remove module loading and superfluous deps

--- activate.sh
+++ activate.sh
@@ -1,17 +1,3 @@
-if [[ $OSTYPE != "darwin"* ]]; then
-  source /etc/profile.d/modules.sh
-
-  module purge
-
-  module load cuda/12.1
-  module load cudnn/v9.0.0.312-cuda.12.3
-  module load NCCL/2.21.5-1-cuda.12.2
-fi
-
-# Load our shared HF token
-export HF_TOKEN=`cat /large_experiments/robust_vlm/abstention-bench/huggingface/hf_token`
-# A shared HF directory created consistent Permission Denied issues.
-# export HF_HOME="/large_experiments/robust_vlm/abstention-bench/huggingface"
 export AZURE_GPT4O_API_KEY=`cat .secrets/azure_gpt4o_api_key`
 export AZURE_O1_PREVIEW_API_KEY=`cat .secrets/azure_o1_preview_api_key`
 export AZURE_O1_API_KEY=`cat .secrets/azure_o1_api_key`
--- setup.sh
+++ setup.sh
@@ -1,13 +1,3 @@
-if [[ $OSTYPE != "darwin"* ]]; then
-  source /etc/profile.d/modules.sh
-
-  module purge
-
-  module load cuda/12.1
-  module load cudnn/v9.0.0.312-cuda.12.3
-  module load NCCL/2.21.5-1-cuda.12.2
-fi
-
 # Create and activate the new environment as per the environment.yml file
 mamba env create -f environment.yml
 
@@ -18,13 +8,9 @@ mamba activate abstention-bench
  
 if [[ $OSTYPE == "darwin"* ]]; then
   pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 -U 
-  mamba install -c pytorch faiss-cpu=1.10.0
 else
   pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 -U --index-url https://download.pytorch.org/whl/cu121
-  mamba install -y -c pytorch -c nvidia faiss-gpu=1.10.0
 fi 
-
-pip install simcse --no-deps
 
 pip install -e .
 
